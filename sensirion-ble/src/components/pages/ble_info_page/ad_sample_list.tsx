f9c41bd83bd236bfa98bf0c869ae351e3748ec2c
Quentin <quentin.fisch@sensirion.com>
2025-04-03 13:20:39 +0200

    filter sample lists

--- sensirion-ble/src/components/pages/ble_info_page/ad_sample_list.tsx
+++ sensirion-ble/src/components/pages/ble_info_page/ad_sample_list.tsx
@@ -4,12 +4,35 @@ import type {SampleTypes} from "../../../types/ble-sample-types-adv-schema";
 import {Accordion} from "radix-ui";
 import {ChevronIcon} from "../../vectors/chevronIcon.tsx";
 import "./accordion.css"
+import {SearchCriterias} from "../../../types/search-criterias.d.tsx";
+import {useContext} from "react";
+import {FilterContext} from "./contexts.tsx";
 
 const advSamples = samples as SampleTypes;
 
 function AdvertisementSampleList() {
+    const fContext = useContext(FilterContext);
+
+    function filterAdvertisementSampleList(filters: SearchCriterias) {
+        let filteredSamples = [...advSamples["sample-types"]]
+        // Filter on "Gadget" select according the suitable-for in sample
+        if (filters.selectedGadget != undefined) {
+            filteredSamples = filteredSamples.filter(s =>
+                s["sample-type"]["suitable-for"]?.includes(filters.selectedGadget!));
+        }
+        // Filter on "Signals" select according the fields in sample
+        if (filters.selectedSignals.length > 0) {
+            const selSign = new Set(filters.selectedSignals);
+            filteredSamples = filteredSamples.filter(s => {
+                const sampleSignals = new Set(s["sample-type"].fields?.map(f => f.field.name));
+                return selSign.isSubsetOf(sampleSignals);
+            });
+        }
+        return filteredSamples;
+    }
+
     return <Accordion.Root type="single">
-        {advSamples["sample-types"].map((s, i) => {
+        {filterAdvertisementSampleList(fContext.filters).map((s, i) => {
             return (
                 <Accordion.Item value={"ad-sample-" + i} className="accordion" key={"ad-sample-" + i}>
                     <Accordion.Header className="accordion__header">
--- sensirion-ble/src/components/pages/ble_info_page/dl_sample_list.tsx
+++ sensirion-ble/src/components/pages/ble_info_page/dl_sample_list.tsx
@@ -3,13 +3,36 @@ import samples from "../../../resources/ble-dl-sample-types.yml";
 import type {SampleTypes} from "../../../types/ble-sample-types-download-schema";
 import {Accordion} from "radix-ui";
 import {ChevronIcon} from "../../vectors/chevronIcon.tsx";
+import {SearchCriterias} from "../../../types/search-criterias.d.tsx";
+import {useContext} from "react";
+import {FilterContext} from "./contexts.tsx";
 
 const dlSamples = samples as SampleTypes;
 
 
 function DownloadSampleList() {
+    const fContext = useContext(FilterContext);
+
+    function filterDownloadSampleList(filters: SearchCriterias) {
+        // Filter on "Gadget" select according the suitable-for in sample
+        let filteredSamples = [...dlSamples["sample-types"]]
+        if (filters.selectedGadget != undefined) {
+            filteredSamples = filteredSamples.filter(s =>
+                s["sample-type"]["suitable-for"]?.includes(filters.selectedGadget!));
+        }
+        // Filter on "Signals" select according the fields in sample
+        if (filters.selectedSignals.length > 0) {
+            const selSign = new Set(filters.selectedSignals);
+            filteredSamples = filteredSamples.filter(s => {
+                const sampleSignals = new Set(s["sample-type"].fields?.map(f => f.field.name));
+                return selSign.isSubsetOf(sampleSignals);
+            });
+        }
+        return filteredSamples;
+    }
+
     return <Accordion.Root type="single">
-        {dlSamples["sample-types"].map((s, i) => {
+        {filterDownloadSampleList(fContext.filters).map((s, i) => {
             return (
                 <Accordion.Item value={"dl-sample-" + i} className="accordion" key={"dl-sample-" + i}>
                     <Accordion.Header className="accordion__header">
