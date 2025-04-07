a473f7c20c09161d6fea5cad9b21591cc1e8d2a5
Quentin <quentin.fisch@sensirion.com>
2025-04-07 20:53:19 +0200

    Converted service list from accordion to dialog

--- sensirion-ble/src/components/pages/ble_info_page/services/services_list.tsx
+++ sensirion-ble/src/components/pages/ble_info_page/services/services_list.tsx
@@ -1,13 +1,12 @@
-import {Accordion} from "radix-ui";
-import {ChevronIcon} from "../../../vectors/chevronIcon.tsx";
-import "../accordion.css"
+import {useContext, useState} from "react";
+import {Dialog} from "radix-ui";
+import "../dialog.css"
 
 // @ts-expect-error: untyped object import
 import services from "../../../../resources/ble-services.yml";
 import type {BLEServiceSchemaDefinition} from "../../../../types/ble-service-schema";
 import ServiceInfoContent from "./service_info_content.tsx";
 import ServiceInfoHeader from "./service_info_header.tsx";
-import {useContext} from "react";
 import {FilterContext} from "../contexts.tsx";
 import {SearchCriterias} from "../../../../types/search-criterias.d.tsx";
 
@@ -15,6 +14,7 @@ const bleServices = services as BLEServiceSchemaDefinition;
 
 function ServicesList() {
     const fContext = useContext(FilterContext);
+    const [selected_service, set_selected_service] = useState(filterServicesList(fContext.filters)[0].service);
 
     // Filter on "Gadget" select according the implemented_by in characteristics list
     function filterServicesList(filters: SearchCriterias) {
@@ -26,30 +26,34 @@ function ServicesList() {
         return bleServices["ble-services"];
     }
 
-    return <Accordion.Root type="single">
-        {filterServicesList(fContext.filters).map((s, i) => {
-            return (
-                <Accordion.Item value={"service-" + i} className="accordion" key={"service-" + i}>
-                    <Accordion.Header className="accordion__header">
-                        <Accordion.Trigger className="accordion__header__trigger">
-                            <ServiceInfoHeader
-                                name={s.service.name}
-                                isLegacy={!s.service["ble-sig-reference"]}
-                                numberOfCharacteristics={s.service["supported-characteristics"].length}
-                                uuid={s.service.uuid}/>
-                            <ChevronIcon
-                                className="accordion__header__chevron"
-                                aria-hidden
-                            />
-                        </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content className="accordion__content">
-                        <ServiceInfoContent content={s.service} />
-                    </Accordion.Content>
-                </Accordion.Item>
-            );
-        })}
-    </Accordion.Root>
+    return <Dialog.Root>
+        <div className="dialog_trigger_list">
+            {filterServicesList(fContext.filters).map((s) => {
+                return <Dialog.Trigger asChild>
+                    <ServiceInfoHeader
+                        name={s.service.name}
+                        isLegacy={!s.service["ble-sig-reference"]}
+                        numberOfCharacteristics={s.service["supported-characteristics"].length}
+                        uuid={s.service.uuid}
+                        onClick={() => set_selected_service(s.service)}
+                        className="dialog_trigger_list__entry"
+                    />
+                </Dialog.Trigger>
+            })}
+        </div>
+
+        <Dialog.Portal>
+            <Dialog.Overlay className="dialog_overlay"/>
+            <Dialog.Content className="dialog_content" aria-describedby={undefined}>
+                <Dialog.Title className="dialog_title">{selected_service.name}</Dialog.Title>
+                <Dialog.Close asChild>
+                    <button className="dialog_close_button" aria-label="Close">X</button>
+                </Dialog.Close>
+                <ServiceInfoContent content={selected_service}/>
+            </Dialog.Content>
+        </Dialog.Portal>
+    </Dialog.Root>
+
 }
 
 
